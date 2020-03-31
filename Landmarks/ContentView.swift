39b9ef1b820e91fc8cebabf6c970f919deee942c
Mohammed Faizuddin <faizuddin.mohammed00@delta.com>
2020-03-31 12:47:07 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Mohammed Faizuddin on 3/31/20.
+//  Copyright © 2020 Mohammed Faizuddin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
